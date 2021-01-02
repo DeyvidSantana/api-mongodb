11caccadfd0786c8523f93074236beba9228e145
Deyvid Santana <sdsdeyvidh@outlook.com>
2021-01-02 20:42:08 -0300

    Initial commit

---
+++ ApiMongoDB/Controllers/InfectadoController.cs
@@ -0,0 +1,58 @@
+using System;
+using ApiMongoDB.Data.Collections;
+using ApiMongoDB.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace ApiMongoDB.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
+    {
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Infectado> _infectadosCollection;
+
+        public InfectadoController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+
+            _infectadosCollection.InsertOne(infectado);
+            
+            return StatusCode(201, "Infectado adicionado com sucesso");
+        }
+
+        [HttpPut]
+        public ActionResult AtualizarInfectado([FromBody] InfectadoDto dto)
+        {
+            _infectadosCollection.UpdateOne(Builders<Infectado>.Filter.Where(_ => _.DataNascimento == dto.DataNascimento), 
+                Builders<Infectado>.Update.Set("sexo", dto.Sexo));
+            
+            return Ok("Atualizado com sucesso");
+        }
+
+        [HttpDelete("{data}")]
+        public ActionResult ExcluirInfectado(DateTime data)
+        {
+            var builder = Builders<Infectado>.Filter.Where(_ => _.DataNascimento == data);
+            _infectadosCollection.DeleteOne(builder);
+            
+            return Ok("Excluído com sucesso");
+        }
+
+        [HttpGet]
+        public ActionResult ObterInfectados()
+        {
+            var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+            
+            return Ok(infectados);
+        }
+    }
+}
